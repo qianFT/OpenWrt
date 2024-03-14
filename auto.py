f42b29e3dd8889431173c41864cb8cbfac719ac8
qianFT <38234358+qianFT@users.noreply.github.com>
2024-03-14 15:41:40 +0800

    Update auto.py

--- auto.py
+++ auto.py
@@ -5,7 +5,9 @@ ids = ['280','346','484','261','229','344','221']
 for i,id_i in enumerate(ids):
 	response = requests.get('http://mpp.liveapi.mgtv.com/v1/epg/turnplay/getLivePlayUrlMPP?version=PCweb_1.0&platform=1&buss_id=2000001&channel_id='+id_i)
 	data = response.json()
+	print(data)
 	text = data['data']['url']
+	
 
 
 	path = 'm3u8/hn'+str(i)+'.m3u8'
